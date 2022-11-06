9570202d1dfdca05190f4e3569677013b674f8d3
emiliollbb <emiliollbb@gmail.com>
2022-11-06 09:40:15 +0100

    Add witch

--- main.c
+++ main.c
@@ -3,27 +3,39 @@
 #include <psv.h>
 #include "bin/background.h"
 #include "bin/sprites.h"
+#include "bin/witch_sprite.h"
 
 sprite player;
+sprite pumpkin;
 
 int main(void) {
     load_background(background);
     clrscr();
     
-    player.resource = &sprites_0_0;
+    pumpkin.resource = &sprites_0_0;
+    pumpkin.x=0;
+    pumpkin.y=10;
+    pumpkin.width = 30;
+    pumpkin.height = 27;
+    calculate_coords(&pumpkin);
+    consoleLogWord(pumpkin.mem);
+    
+    waitFrames(10);
+    draw_sprite(&pumpkin);
+    
+    
+    player.resource = &witch_sprite_0_0;
     player.x=0;
     player.y=50;
-    player.width = 30;
-    player.height = 27;
+    player.width = 32;
+    player.height = 37;
     calculate_coords(&player);
-    consoleLogWord(player.mem);
-    
     waitFrames(10);
     draw_sprite(&player);
     
     while(1) {
         waitFrames(10);
-        move_sprite_right(&player);
+        move_sprite_right(&pumpkin);
     }
     
         
